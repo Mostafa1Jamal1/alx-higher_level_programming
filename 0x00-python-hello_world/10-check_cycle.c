e3e2cfc76bc7f17fc8f2aed8d3b7300858e91e14
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-09-05 01:10:35 +0300

    modified:   10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,8 +8,11 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list;
+	listint_t *current;
 
+	if (list == NULL)
+		return (0);
+	current = list;
 	while (current != NULL)
 	{
 		current = current->next;
