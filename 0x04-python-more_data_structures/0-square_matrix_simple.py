bade17045ff0cdac83a470f8c53168b598780eef
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-09-13 19:27:33 +0300

    style :  modified:   0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     squar = []
-    get_sq = lambda x : x ** 2
     for alist in matrix:
-        squar.append(list(get_sq(x) for x in alist))
+        squar.append(list((lambda x: x ** 2)(x) for x in alist))
     return squar
