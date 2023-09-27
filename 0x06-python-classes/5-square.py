d477d5de4f2cf08cd90ed84d402f1c0d9e08445c
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-09-27 04:20:07 +0300

    Task5 in 0x06.Python-Classes and Objects project and modified README.md

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''Just class `Square` that defines a square'''
+
+
+class Square:
+    '''In this class
+    Private instance attribute: size
+    '''
+
+    def __init__(self, size=0):
+        ''' Instantiation with size optional
+        Args:
+            size (int): the size of the aquare, must be >= 0
+        '''
+        self.size = size
+
+    def area(self):
+        '''Get the area of the square
+
+        Returns:
+            the area of the square
+        '''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''Print the area of the square object with #s'''
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if self.__size == 0:
+            print()
+
+    @property
+    def size(self):
+        '''the getter method of class Square
+        the size of the aquare, must be >= 0 and of course int type
+        '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
