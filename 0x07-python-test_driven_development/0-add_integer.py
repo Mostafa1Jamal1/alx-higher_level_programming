e7021e4e960133dcf01541c7f4cbad13a14f6aba
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-07 05:29:27 +0300

    forgot a

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,5 +14,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    else
+    else:
         return int(a) + int(b)
