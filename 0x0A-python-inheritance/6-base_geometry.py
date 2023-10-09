5d04cacc4366e55ef2a44cd5b665d3a01c42b393
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-10 00:18:11 +0300

    Task6 in 0x0A.Python-Inheritance project and modified README.md

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Module of one class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''A class with only one public instance method:
+        def area(self): that raises an Exception with the message
+        area() is not implemented'''
+    
+    def area(self):
+        '''raises an Exception with the message area() is not implemented'''
+        raise Exception('area() is not implemented')
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -42,4 +42,11 @@ You are not allowed to import any module
 
 `5-base_geometry.py` -> an empty class BaseGeometry.
 
+
+
+`6-base_geometry.py` -> a class BaseGeometry (based on 5-base_geometry.py).
+
+Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+You are not allowed to import any module
+
  
