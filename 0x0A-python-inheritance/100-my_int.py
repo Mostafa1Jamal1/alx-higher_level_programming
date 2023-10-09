e4e6b34b4c3637b26a1509f1d7a798e2fa18b927
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-10 02:01:35 +0300

    style: pycodestyle 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,7 +4,7 @@
 
 class MyInt(int):
     '''MyInt is a rebel. MyInt has == and != operators inverted'''
-    
+
     def __eq__(self, value):
         '''Return self != value.'''
         return super().__ne__(value)
