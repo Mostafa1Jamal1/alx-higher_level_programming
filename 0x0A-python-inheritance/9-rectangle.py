0c6db8bf85579ff637275c420011cb0b70a8832c
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-10 01:28:13 +0300

    Task9 in 0x0A.Python-Inheritance project and modified README.md

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module of one class Rectangle that inherts from BaseGeometry'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''A subclass from BaseGeometry that:
+    Instantiation with width and height'''
+
+    def __init__(self, width, height):
+        '''Instantiation with width and height'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+        
+    def __str__(self):
+        '''return, the following rectangle description:
+        [Rectangle] <width>/<height>'''
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        '''Returns the area of the Rectangle'''
+        return self.__width * self.__height
