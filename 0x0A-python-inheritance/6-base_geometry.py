a8833f5c9f4b341ec968e8dd41157ec652a2c366
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-10 00:19:17 +0300

    style: pycodestyle 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     '''A class with only one public instance method:
         def area(self): that raises an Exception with the message
         area() is not implemented'''
-    
+
     def area(self):
         '''raises an Exception with the message area() is not implemented'''
         raise Exception('area() is not implemented')
