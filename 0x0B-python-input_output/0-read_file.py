3aa7b27fbaa699981fe1805440a0945c761e6131
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-10 20:04:30 +0300

    modified:   0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     '''reads a text file (UTF8) and prints it to stdout'''
     with open(filename, encoding="utf-8") as afile:
-        print(afile.read())
+        print(afile.read(), end="")
