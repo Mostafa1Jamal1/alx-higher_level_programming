2fc7e9d76490cf392f6bb7b8c911b0e089d9ed26
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-11 16:39:54 +0300

    modified:   101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 '''Module of Log parsing script'''
 
-import sys
 
-numline = 0
-totalSize = 0
-stcodes = {"200": 0, "301": 0, "400": 0,
-           "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+if __name__ == "__main__":
+    import sys
 
-while True:
-    try:
-        for line in sys.stdin:
-            linelist = line.split(" ")
-            totalSize += int(linelist[8])
-            stcodes[linelist[7]] += 1
+    numline = 0
+    totalSize = 0
+    stcodes = {"200": 0, "301": 0, "400": 0,
+               "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
-            numline += 1
-            if numline == 10:
-                numline = 0
-                print("File size: {}".format(totalSize))
-                for code in sorted(stcodes.keys()):
-                    if stcodes[code] != 0:
-                        print("{}: {}".format(code, stcodes[code]))
-    except KeyboardInterrupt:
-        print("File size: {}".format(totalSize))
-        for code in sorted(stcodes.keys()):
-            if stcodes[code] != 0:
-                print("{}: {}".format(code, stcodes[code]))
-        exit()
+    while True:
+        try:
+            for line in sys.stdin:
+                linelist = line.split(" ")
+                totalSize += int(linelist[8])
+                stcodes[linelist[7]] += 1
+
+                numline += 1
+                if numline == 10:
+                    numline = 0
+                    print("File size: {}".format(totalSize))
+                    for code in sorted(stcodes.keys()):
+                        if stcodes[code] != 0:
+                            print("{}: {}".format(code, stcodes[code]))
+        except KeyboardInterrupt:
+            print("File size: {}".format(totalSize))
+            for code in sorted(stcodes.keys()):
+                if stcodes[code] != 0:
+                    print("{}: {}".format(code, stcodes[code]))
+            exit()
