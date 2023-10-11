e29212f5850773598695f3876c3f85a8c164119c
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-11 16:18:15 +0300

    Task14 in 0x0B.Python-Input/Output project

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Module of Log parsing script'''
+
+import sys
+
+numline = 0
+totalSize = 0
+stcodes = {"200": 0, "301": 0, "400": 0,
+        "401": 0, "403": 0, "404": 0,
+        "405": 0, "500": 0}
+
+while True:
+    try:
+        for line in sys.stdin:
+            linelist = line.split(" ")
+            totalSize += int(linelist[8])
+            stcodes[linelist[7]] += 1
+
+            numline += 1
+            if numline == 10:
+                numline = 0
+                print("File size: {}".format(totalSize))
+                for code in sorted(stcodes.keys()):
+                    if stcodes[code] != 0:
+                        print("{}: {}".format(code, stcodes[code]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(totalSize))
+        for code in sorted(stcodes.keys()):
+            if stcodes[code] != 0:
+                print("{}: {}".format(code, stcodes[code]))
+        exit()
