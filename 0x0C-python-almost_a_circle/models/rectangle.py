556fc8e32c9b28865cef046a7950ab9c690ce174
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-15 19:07:17 +0300

    Task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,9 @@ class Rectangle(Base):
     def area(self):
         '''returns the rectangle area'''
         return self.__width * self.__height
+
+    def display(self):
+        '''prints in stdout the Rectangle instance with the character #'''
+        for row in range(self.__height):
+            print("#" * self.__width)
+
