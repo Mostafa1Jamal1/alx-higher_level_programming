d31e4003f2b9198224b9c7e7d1a8f63aa1e086e7
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-15 19:55:58 +0300

    task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,15 @@ class Rectangle(Base):
                 print(" " * self.__x, end="")
             print("#" * self.__width)
 
+    def update(self, *args):
+        '''assigns an argument to each attribute'''
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
