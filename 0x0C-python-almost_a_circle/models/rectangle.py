3168a6977568066c42e78e4339a17c161f003652
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-16 03:15:45 +0300

    refactor models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,13 +89,8 @@ class Rectangle(Base):
 
     def update(self, *args):
         '''assigns an argument to each attribute'''
+        attrs = ("id", "width", "height", "x", "y")
         if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+            argv = args[:5] #To handle if more than 4 arguments passed
+            for i, arg in enumerate(argv):
+                setattr(self, attrs[i], arg)
