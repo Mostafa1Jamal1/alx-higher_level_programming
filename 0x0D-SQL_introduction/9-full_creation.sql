680ad9fb155ff18aa503d5c826cb400b6384da81
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-11-14 21:33:30 +0200

    try fix red checks task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -11,4 +11,4 @@
         -- `id` = 3, `name` = “Bob”, `score` = 14
         -- `id` = 4, `name` = “George”, `score` = 8
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (1, 'Jone', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table VALUES (1, 'Jone', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8)
