078bb50de50e41d69d04f1b0814daf3149e5bdd2
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2023-12-12 00:59:48 +0200

    fix: try3

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const argv = process.argv.slice(2);
 if (argv.length < 2) {
   console.log(0);
 } else {
-  let biggest = Number(argv[1]);
+  let biggest = Number(argv[0]);
   let secBig = Number(argv[0]);
   for (const i of argv) {
     const num = Number(i);
