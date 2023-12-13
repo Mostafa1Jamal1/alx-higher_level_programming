eb06ac860e272e256660144363f03019ed5571f5
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2023-12-13 02:47:28 +0200

    refactor: 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,15 +3,14 @@
 const dict = require('./101-data.js').dict;
 
 const mydict = {};
-// create pairs of occurrences and empty list
+
 for (const UserId in dict) {
   const occurrences = dict[UserId];
-  mydict[occurrences] = [];
-}
-// add to the empty list the user id
-for (const UserId in dict) {
-  const occurrences = dict[UserId];
+
+  if (!(occurrences in mydict)) {
+    mydict[occurrences] = [];
+  }
+
   mydict[occurrences].push(UserId);
 }
-
 console.log(mydict);
