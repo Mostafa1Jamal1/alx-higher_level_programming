d812a77d33927668e4f90289baa251cfde6e3ef7
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2023-12-16 18:15:34 +0200

    fix: red Check try 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     dbname = sys.argv[3]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                           passwd=password, db=dbname, charset="utf8")
+                           passwd=password, db=dbname, charset="utf8mb4")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states\
-                WHERE name LIKE 'N_%'\
+                WHERE name LIKE 'N_%' COLLATE utf8mb4_ja_0900_as_cs\
                 ORDER BY id")
     query_rows = cur.fetchall()
     for row in query_rows:
