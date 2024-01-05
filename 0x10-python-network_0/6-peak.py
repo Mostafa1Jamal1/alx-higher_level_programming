9f978afd67b4752fceba2f56041020b39db67de6
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-01-06 01:46:01 +0200

    style: pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,8 +3,8 @@
 
 
 def find_peak(list_of_integers):
-   '''a function that finds a peak in a list of unsorted integers.'''
-   if list_of_integers:
-      return  max(list_of_integers)
-   else:
-      return None
+    '''a function that finds a peak in a list of unsorted integers.'''
+    if list_of_integers:
+        return max(list_of_integers)
+    else:
+        return None
