0ca012f35fbcfd3cf055215e5e4581ca37af8edd
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-01-07 20:54:11 +0200

    fix: try 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' takes in a letter and sends a POST request
-to http://36595c7ea186.6ad0eb84.alx-cod.online:5000/search_user
+to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 for more information see README.md'''
 import requests
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
 
     url = 'http://0.0.0.0:5000/search_user'
-    data = {'q': sys.argv[1] if len(sys.argv) > 1 else ""}
+    data = {'q': sys.argv[1][0] if len(sys.argv) > 1 else ""}
 
     r = requests.post(url, data=data)
 
@@ -20,5 +20,5 @@ if __name__ == "__main__":
             print(f"[{jsond.get('id')}] {jsond.get('name')}")
         else:
             print("No result")
-    except requests.exceptions.JSONDecodeError as e:
+    except Exception:
         print("Not a valid JSON")
