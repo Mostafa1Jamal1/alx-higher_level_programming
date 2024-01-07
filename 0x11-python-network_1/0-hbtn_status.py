94fd0ba0d9637247a1c7ba5c958f5be0502b3945
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-01-07 03:49:45 +0200

    style: pycodestyle and encodeing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,11 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
         content = response.read()
         print(f'''Body response:
         \t- type: {type(content)}
         \t- content: {content}
-        \t- utf8 content: OK''')
+        \t- utf8 content: {content.decode('utf-8')}''')
