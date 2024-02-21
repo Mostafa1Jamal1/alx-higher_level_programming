fe20260855a7bbe3043abc0ef21fa2a93379cd4b
Mostafa <mostafa.jamal.mjm@gmail.com>
2024-02-20 18:17:01 -0800

    refact: improve readability

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,13 +6,13 @@ request(process.argv[2], function (err, response, body) {
   if (!err) {
     const TaskList = JSON.parse(body);
     const result = {};
-    for (const i in TaskList) {
-      const task = TaskList[i];
-      if (!(String(task.userId) in result)) {
-        result[String(task.userId)] = 0;
+    for (const task of TaskList) {
+      const userId = String(task.userId);
+      if (!(userId in result)) {
+        result[userId] = 0;
       }
       if (task.completed) {
-        result[String(task.userId)] += 1;
+        result[userId] += 1;
       }
     }
     console.log(result);
